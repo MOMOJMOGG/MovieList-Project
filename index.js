887f2bb74aeb72e0f46a5e44d9e5e8bd6fd62e0a
MOMOJMOGG <momomacaca@gmail.com>
2021-03-14 22:05:15 +0800

    Add search submit event

--- index.js
+++ index.js
@@ -4,31 +4,61 @@ const POSTER_URL = BASE_URL + '/posters/'
 
 const movies = []
 const dataPanel = document.querySelector('#data-panel')
+const searchForm = document.querySelector('#search-form')
+const searchInput = document.querySelector('#search-input')
+
+// 監聽表單提交事件
+searchForm.addEventListener('submit', function onSearchFormSubmitted(event) {
+  // 終止元件的預設行為
+  event.preventDefault()
+
+  // 取得搜尋關鍵字
+  const keyword = searchInput.value.trim().toLowerCase()
+  console.log(keyword)
+  // 錯誤處理：輸入無效字串
+  if (!keyword.length) {
+    return alert('請輸入有效字串')
+  }
+
+  // 儲存符合篩選標準的電影
+  let filterMovies = []
+
+  // 條件篩選
+  filterMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(keyword)
+  )
+
+  // 錯誤處理 : 無符合條件的結果
+  if (filterMovies.length === 0) {
+    return alert(`您輸入的關鍵字 : ${keyword} 沒有符合條件的電影!`)
+  }
+
+  // 重新更新至畫面
+  renderMovieList(filterMovies)
+})
 
 // 監聽 data panel
 dataPanel.addEventListener('click', function onPanelClicked(event) {
   if (event.target.matches('.btn-show-movie')) {
-    showMovieModal(event.target.dataset.id)
+    showMovieModal(Number(event.target.dataset.id))
   }
 })
 
 // Modal 顯示
 function showMovieModal(id) {
+  function matchIdFromList(movie) {
+    return movie.id === id
+  }
   const modalTitle = document.querySelector('#movie-modal-title')
   const modalImage = document.querySelector('#movie-modal-image')
   const modalDate = document.querySelector('#movie-modal-date')
   const modalDescription = document.querySelector('#movie-modal-description')
 
-  axios
-    .get(INDEX_URL + id)
-    .then((response) => {
-      const data = response.data.results
-      modalTitle.innerText = data.title
-      modalDate.innerText = 'Release date: ' + data.release_date
-      modalDescription.innerText = data.description
-      modalImage.innerHTML = `<img src="${POSTER_URL + data.image}" alt="movie-poster" class="img-fluid">`
-    })
-
+  const targetMovie = movies.find(matchIdFromList)
+  modalTitle.innerText = targetMovie.title
+  modalDate.innerText = 'Release date: ' + targetMovie.release_date
+  modalDescription.innerText = targetMovie.description
+  modalImage.innerHTML = `<img src="${POSTER_URL + targetMovie.image}" alt="movie-poster" class="img-fluid">`
 }
 
 // 放資料進網頁
