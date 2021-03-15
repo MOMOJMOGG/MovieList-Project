5a72bac065190d6de98ae93c99187d647a469d1e
MOMOJMOGG <momomacaca@gmail.com>
2021-03-16 00:07:27 +0800

    Reorganize Code

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// ***************************************************************************** Parameters
 const BASE_URL = 'https://movie-list.alphacamp.io'
 const INDEX_URL = BASE_URL + '/api/v1/movies/'
 const POSTER_URL = BASE_URL + '/posters/'
@@ -10,94 +11,66 @@ const searchForm = document.querySelector('#search-form')
 const searchInput = document.querySelector('#search-input')
 const paginator = document.querySelector('#paginator')
 
-// 切割部分電影資料
+// ***************************************************************************** Function
+/*** 切割部分電影資料 ***/
 function getMoviesByPage(page) {
-  // 判斷要取搜尋清單 或是 總電影清單
+  // 判斷要取 搜尋清單 或是 總電影清單
   const targetList = filteredMovies.length ? filteredMovies : movies
 
-  //計算起始 index 
+  // 計算起始 index 
   const startIndex = (page - 1) * MOVIES_PER_PAGE
 
-  //回傳切割後的新陣列
+  // 回傳切割後的新陣列
   return targetList.slice(startIndex, startIndex + MOVIES_PER_PAGE)
 }
 
-// 監聽表單提交事件
-searchForm.addEventListener('submit', function onSearchFormSubmitted(event) {
-  // 終止元件的預設行為
-  event.preventDefault()
-
-  // 取得搜尋關鍵字
-  const keyword = searchInput.value.trim().toLowerCase()
-
-  // 條件篩選
-  filteredMovies = movies.filter((movie) =>
-    movie.title.toLowerCase().includes(keyword)
-  )
-
-  // 錯誤處理 : 無符合條件的結果
-  if (filteredMovies.length === 0) {
-    return alert(`您輸入的關鍵字 : ${keyword} 沒有符合條件的電影!`)
-  }
-
-  // 重新更新至畫面
-  renderPaginator(filteredMovies.length)
-  renderMovieList(getMoviesByPage(1))
-})
-
-// 新增喜歡清單
+/*** 新增 收藏清單 ***/
 function addToFavorite(id) {
+  // 比對函式
   function matchIdFromList(movie) {
     return movie.id === id
   }
+
+  // 從 local Storage 取得 收藏清單 資料
   const list = JSON.parse(localStorage.getItem('favoriteMovies')) || []
+
+  // 從 電影總清單 取出第 id 筆電影
   const movie = movies.find(matchIdFromList)
 
+  // 錯誤處理 : 重複收藏
   if (list.some(matchIdFromList)) {
     return alert('此電影已經在收藏清單中！')
   }
 
+  // 加入 收藏清單 並 更新 local Storage
   list.push(movie)
   localStorage.setItem('favoriteMovies', JSON.stringify(list))
 }
 
-// Modal 顯示
+/***  Modal 顯示 ***/
 function showMovieModal(id) {
+  // 比對函式
   function matchIdFromList(movie) {
     return movie.id === id
   }
+
+  // Modal DOM 元素
   const modalTitle = document.querySelector('#movie-modal-title')
   const modalImage = document.querySelector('#movie-modal-image')
   const modalDate = document.querySelector('#movie-modal-date')
   const modalDescription = document.querySelector('#movie-modal-description')
 
+  // 從 電影總清單 取出第 id 筆電影
   const targetMovie = movies.find(matchIdFromList)
+
+  // 設定 顯示資訊
   modalTitle.innerText = targetMovie.title
   modalDate.innerText = 'Release date: ' + targetMovie.release_date
   modalDescription.innerText = targetMovie.description
   modalImage.innerHTML = `<img src="${POSTER_URL + targetMovie.image}" alt="movie-poster" class="img-fluid">`
 }
 
-// favorite Button
-
-
-// 監聽 data panel
-dataPanel.addEventListener('click', function onPanelClicked(event) {
-  const target = event.target
-  if (target.matches('.btn-show-movie')) {
-    showMovieModal(Number(event.target.dataset.id))
-  } else if (target.matches('.btn-add-favorite')) {
-    // 加到 local Storage 收藏清單
-    addToFavorite(Number(target.dataset.id))
-
-    // 更改成 收藏按鈕
-    const parent = target.parentElement
-    target.remove()
-    const favoriteBtn = `<button class="btn btn-add-favorite" data-id="${target.dataset.id}" style="background-color:HotPink"><i class="far fa-heart fa-lg"></i></button>`
-    parent.innerHTML += favoriteBtn
-  }
-})
-
+/*** 取得 收藏id清單 ***/
 function getFavoriteIdList() {
   const list = JSON.parse(localStorage.getItem('favoriteMovies')) || []
   const idList = []
@@ -107,10 +80,10 @@ function getFavoriteIdList() {
   return idList
 }
 
-// 放資料進網頁
+/*** 更新 電影清單 畫面 ***/
 function renderMovieList(data) {
+  // 取得 收藏id清單
   const favoriteIdList = getFavoriteIdList()
-  console.log(favoriteIdList)
   let contentHTML = ``
 
   data.forEach((item) => {
@@ -127,6 +100,7 @@ function renderMovieList(data) {
                 <button class="btn btn-primary btn-show-movie" data-toggle="modal"
                   data-target="#movie-modal" data-id="${item.id}">More</button>
       `
+    // 判斷顯示 喜歡按鈕 或 加入按鈕
     if (favoriteIdList.some((favoriteId) => favoriteId === item.id)) {
       contentHTML += `<button class="btn btn-favorite" data-id="${item.id}" style="background-color:HotPink"><i class="far fa-heart fa-lg"></i></button>`
     } else {
@@ -144,7 +118,7 @@ function renderMovieList(data) {
   dataPanel.innerHTML = contentHTML
 }
 
-// 更新分頁頁數畫面
+/*** 更新 分頁頁數 畫面 ***/
 function renderPaginator(amount) {
   // 分頁數若為0, 不顯示分頁
   if (amount === 0) {
@@ -152,23 +126,76 @@ function renderPaginator(amount) {
     return
   }
 
-  //計算總頁數
+  // 計算總頁數
   const numberOfPages = Math.ceil(amount / MOVIES_PER_PAGE)
 
-  //製作 template 
+  // 製作分頁
   let contentHTML = ''
-
   for (let page = 1; page <= numberOfPages; page++) {
     contentHTML += `<li class="page-item"><a class="page-link" href="#" data-page="${page}">${page}</a></li>`
   }
-
-  //放回 HTML
   paginator.innerHTML = contentHTML
 }
 
-// 分頁事件監聽
+// ***************************************************************************** Event Listener
+/***  監聽表單 提交 事件 ***/
+searchForm.addEventListener('submit', function onSearchFormSubmitted(event) {
+  // 終止元件的預設行為
+  event.preventDefault()
+
+  // 取得搜尋關鍵字
+  const keyword = searchInput.value.trim().toLowerCase()
+
+  // 條件篩選
+  filteredMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(keyword)
+  )
+
+  // 錯誤處理 : 無符合條件的結果
+  if (filteredMovies.length === 0) {
+    return alert(`您輸入的關鍵字 : ${keyword} 沒有符合條件的電影!`)
+  }
+
+  // 重新更新 分頁 與 電影清單 至畫面
+  renderPaginator(filteredMovies.length)
+  renderMovieList(getMoviesByPage(1))
+})
+
+/*** 監聽dataPanel 按下 事件 ***/
+dataPanel.addEventListener('click', function onPanelClicked(event) {
+  const target = event.target
+
+  // 按鈕判斷 : .btn-show-movie, .btn-add-favorite, .btn-favorite
+  if (target.matches('.btn-show-movie')) {
+    // Modal 顯示
+    showMovieModal(Number(target.dataset.id))
+  } else if (target.matches('.btn-add-favorite')) {
+    // 加到 local Storage 收藏清單
+    addToFavorite(Number(target.dataset.id))
+
+    // 刪除 加入按鈕 更改成 收藏按鈕
+    const parent = target.parentElement
+    target.remove()
+    const favoriteBtn = `<button class="btn btn-favorite" data-id="${target.dataset.id}" style="background-color:HotPink"><i class="far fa-heart fa-lg"></i></button>`
+    parent.innerHTML += favoriteBtn
+  } else if (target.matches('.btn-favorite')) {
+    // 從 收藏清單 移除 該筆電影
+    const list = JSON.parse(localStorage.getItem('favoriteMovies')) || []
+    const movieIndex = list.findIndex((movie) => movie.id === Number(target.dataset.id))
+    list.splice(movieIndex, 1)
+    localStorage.setItem('favoriteMovies', JSON.stringify(list))
+
+    // 刪除 收藏按鈕 更改成 加入按鈕
+    const parent = target.parentElement
+    target.remove()
+    const addBtn = `<button class="btn btn-info btn-add-favorite" data-id="${target.dataset.id}">+</button>`
+    parent.innerHTML += addBtn
+  }
+})
+
+/*** 監聽分頁 按鈕 事件  ***/
 paginator.addEventListener('click', function onPaginatorClicked(event) {
-  //如果被點擊的不是 a 標籤 則 跳出函式
+  // 錯誤處理 : 如果被點擊的不是 a 標籤 則 跳出函式
   if (event.target.tagName !== 'A') return
 
   //透過 dataset 取得被點擊的頁數
@@ -187,6 +214,8 @@ axios
   .then((response) => {
     // ... 為展開運算子
     movies.push(...response.data.results)
+
+    // 初始更新畫面
     renderPaginator(movies.length)
     renderMovieList(getMoviesByPage(1))
     localStorage.setItem('homeCurrentPage', JSON.stringify(1))
